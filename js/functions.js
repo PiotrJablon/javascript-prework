4c08929d85c8b917a0badd1dcfc21c4804cefca2
Piotrek <pjablonski123.00@wp.pl>
2021-01-09 18:15:45 +0100

    computer moves

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,23 @@
+printMessage('Twój ruch to: ' + playerMove);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+  else if(randomNumber == 2){
+  	computerMove = 'papier';
+  }
+  else if(randomNumber == 3){
+  	computerMove = 'nożyce';
+  }
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -13,5 +33,3 @@ if(playerInput == '1'){
   	playerMove = 'nożyce';
   } 
 }
-
-printMessage('Twój ruch to: ' + playerMove);
